aaad7f12432c7d122cd9604fe1e5bc496c7f9c59
grullonvictor06 <grullonvictor06@gmail.com>
2022-07-03 02:05:17 -0400

    added first multiplier

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,9 +1,11 @@
 class DonutMaker {
 
-    constructor(numDonuts, numAutoClickers,costOfautoclicker){
+    constructor(numDonuts, numAutoClickers,costOfautoclicker,numMultiplier,costOfMultiplier){
      this.numDonuts = numDonuts;
      this.numAutoclickers = numAutoClickers;
      this.costOfautoclicker = costOfautoclicker;
+     this.numMultiplier = numMultiplier;
+     this.costOfMultiplier = costOfMultiplier;
     }
 
     addDonut(){
@@ -11,7 +13,7 @@ class DonutMaker {
     }
 
     addAutoClicker(){
-        if(this.numDonuts >= this.costOfautoclicker){
+        if(this.numDonuts >= 100){
             this.numDonuts -= this.costOfautoclicker;
             this.numAutoclickers += 1;
         }
@@ -21,10 +23,29 @@ class DonutMaker {
     }
 
     addDonutByAutoClicker(){
-        this.numDonuts += this.numAutoclickers*5;
+        this.numDonuts += this.numAutoclickers;
     }
 
-    
+    addMultiplier(){
+        if(this.numDonuts >= 10){
+            this.numMultiplier += 1;
+            this.numDonuts -= this.costOfMultiplier;
+        }
+        if(this.numMultiplier >= 1){
+            this.costOfMultiplier += this.costOfMultiplier*0.1
+        }
+        if(this.numMultiplier >= 1){
+            this.numDonuts += 1*1.2;
+        
+        }
+        
+    }
+
+    // addDonutsByMultiplier(){
+    //     this.numDonuts += 1*1.2;
+    // }
+
+
 }
 
 export default DonutMaker;
--- DonutMaker.test.js
+++ DonutMaker.test.js
@@ -40,14 +40,46 @@ describe("DonutMaker", () => {
         expect(underTest.costOfautoclicker).toEqual(121);
     })
 
-    test("should be able to increase more donuts by autoclicker times 5", () => {
+    test("should be able to increase more donuts by autoclicker", () => {
         const underTest = new DonutMaker(100,10,100);
         underTest.addDonutByAutoClicker();
-        expect(underTest.numDonuts).toEqual(150);
+        expect(underTest.numDonuts).toEqual(110);
     })
 
-    test("should be able to add a mulitplier with 200 donuts", () => {
-        const underTest = new DonutMaker()
-        underTest.
+    test("should be able to add a multiplier", () => {
+        const underTest = new DonutMaker(200,1,100,0);
+        underTest.addMultiplier();
+        expect(underTest.numMultiplier).toEqual(1);
+    })
+
+    test("should be able to increase the cost of multiplier by 10% after each purchase", () => {
+        const underTest = new DonutMaker (200,1,100,1,100);
+        underTest.addMultiplier();
+        expect(underTest.costOfMultiplier).toEqual(110);
+    })
+
+    test("should be able to increase the cost of multiplier by 10% after 2nd purchase", () => {
+        const underTest = new DonutMaker (200,2,100,2,110);
+        underTest.addMultiplier();
+        expect(underTest.costOfMultiplier).toEqual(121);
+    })
+
+    test("should Not be able to purchase a multiplier with 199 donuts", () =>{
+        const underTest = new DonutMaker (9,0,100,0,10);
+        underTest.addMultiplier();
+        expect(underTest.numDonuts).toEqual(9);
+        expect(underTest.numMultiplier).toEqual(0);
+    })
+
+    test("should Multiply donuts by 1.2 for every donut added with clicker", () => {
+        const underTest = new DonutMaker (110,0,100,1,10);
+        underTest.addMultiplier();
+        expect(underTest.numDonuts).toEqual(101.2);
+    })
+
+    test("should Multiply donuts by 1.2 by the power of 2 for every donut added with clicker", () => {
+        const underTest = new DonutMaker (110,0,100,2,10);
+        underTest.addMultiplier();
+        expect(underTest.numDonuts).toEqual(101.44);
     })
 });
